9f17ee621c95f3a73837f46095b329b40df3a8c4
farid maghraoui <ferid.maghraoui@gmail.com>
2024-03-15 15:22:28 +0100

    API versionning, URL Based : api/v1

--- stroemer_test_farid/urls.py
+++ stroemer_test_farid/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('blog.urls')),
+    path('api/v1/', include('blog.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
 
