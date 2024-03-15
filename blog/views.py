5c46de03be3456430064fe642f8d9d1921406552
farid maghraoui <ferid.maghraoui@gmail.com>
2024-03-15 14:37:17 +0100

    add pagination + pip freeze

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,19 @@
 from rest_framework import generics
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from rest_framework.pagination import PageNumberPagination
+
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 5  # Number of items per page
+    page_size_query_param = 'page_size'  # URL query parameter to specify page size
+    max_page_size = 100  # Maximum number of items per page
 
 
 class PostListCreateAPIView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = CustomPageNumberPagination
 
     def perform_create(self, serializer):
         # Set the user field to the specific user ID provided by the assignment
@@ -20,6 +28,7 @@ class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
 class CommentListCreateAPIView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    pagination_class = CustomPageNumberPagination
 
 
 class CommentRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
