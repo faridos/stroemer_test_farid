e5e534042d2c342f00e297d520452bef5d46d8e7
farid maghraoui <ferid.maghraoui@gmail.com>
2024-03-15 15:29:08 +0100

    add docs , drf-yasg

--- stroemer_test_farid/settings.py
+++ stroemer_test_farid/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'blog'
+    'drf_yasg',
+    # my apps
+    'blog',
+
 ]
 
 MIDDLEWARE = [
--- stroemer_test_farid/urls.py
+++ stroemer_test_farid/urls.py
@@ -14,18 +14,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.example.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('api/v1/', include('blog.urls')),
-    path('api-auth/', include('rest_framework.urls'))
-]
+    # Other URL patterns...
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
+]
+urlpatterns = [
+    path('api/v1/', include('blog.urls')),
+    #path('api-auth/', include('rest_framework.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
